344353b0e00515c1874a3c1d160df8eca502f314
Hassan Rauf <hassanrauf@ncloud.hk>
2019-11-07 10:18:20 +0500

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new Material(
-          color: Colors.deepOrange,
-          child: new Center(
-            child: new Text(
-              "Hello World",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(
-                  fontWeight: FontWeight.w800, fontStyle: FontStyle.italic),
-            ),
-          ))
+  runApp(new MaterialApp(
+    title: "Welcome App",
+    home: new Welcome(),
+  )
 
 //      new Center(
 //    child: new Text(
@@ -19,4 +13,23 @@ void main() {
 //    ),
 //  )
       );
+}
+
+class Welcome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Material(
+        color: Colors.blue,
+        child: new Center(
+          child: new Text(
+            "Welcome Home",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                fontWeight: FontWeight.w800,
+                fontStyle: FontStyle.italic,
+                fontSize: 25.4),
+          ),
+        ));
+  }
 }
